7e030cdfeca5553caae91f1cdab1522cc7c7c7e5
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-01-14 07:45:30 +0200

    2.3. Add http status-codes

--- src/constants.js
+++ src/constants.js
@@ -3,10 +3,17 @@
 
 module.exports = {
   DEFAULT_COMMAND: `--help`,
-  USER_ARGV_INDEX: 2,
   ExitCode: {
     ERROR: 1,
     SUCCESS: 0,
   },
+  HttpCode: {
+    OK: 200,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+    FORBIDDEN: 403,
+    UNAUTHORIZED: 401,
+  },
   MILLISECONDS_IN_MONTH: 1000 * 60 * 60 * 24 * 30,
+  USER_ARGV_INDEX: 2,
 };
