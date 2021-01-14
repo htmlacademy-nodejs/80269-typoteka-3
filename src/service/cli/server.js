a402fb7faf97b1717bb66a402eb188aadc510017
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-01-14 06:37:27 +0200

    2.3. Add http-server and default port fallback

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -5,9 +5,27 @@ const fs = require(`fs`).promises;
 const http = require(`http`);
 
 
+const DEFAULT_PORT = 3000;
+
+
 module.exports = {
   name: `--server`,
   run(args) {
-    console.log(`Blank server command`);
+    const [customPort] = args;
+    const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+
+    const onClientConnect = () => {
+
+    };
+
+    http.createServer(onClientConnect)
+      .listen(port)
+      .on(`listening`, (err) => {
+        if (err) {
+          console.error(chalk.red(`Ошибка при создании сервера: ${err}`));
+        }
+
+        return console.info(chalk.green(`Ожидаю соединений на порт #${port}`));
+      });
   }
 };
