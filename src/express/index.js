303ad3cf0d2bd0f61a319c91db4bd90b8ff0943b
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-03-01 02:48:19 +0200

    3.1. Add my routes

--- src/express/index.js
+++ src/express/index.js
@@ -2,12 +2,14 @@
 
 const express = require(`express`);
 const mainRoutes = require(`./routes/main.js`);
+const myRoutes = require(`./routes/my.js`);
 
 
 const DEFAULT_PORT = 8080;
 const app = express();
 
 app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
 
 app.listen(DEFAULT_PORT, () => {
   console.log(`Server runs on port: ${DEFAULT_PORT}`);
