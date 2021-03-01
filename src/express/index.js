34089803add58bf83aa1d1edae1201d96d7b4154
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-03-01 02:54:53 +0200

    3.1. Add articles routes

--- src/express/index.js
+++ src/express/index.js
@@ -3,6 +3,7 @@
 const express = require(`express`);
 const mainRoutes = require(`./routes/main.js`);
 const myRoutes = require(`./routes/my.js`);
+const articlesRoutes = require(`./routes/articles.js`);
 
 
 const DEFAULT_PORT = 8080;
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
 
 app.listen(DEFAULT_PORT, () => {
   console.log(`Server runs on port: ${DEFAULT_PORT}`);
