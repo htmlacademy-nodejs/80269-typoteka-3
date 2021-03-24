014c641de2a91d6d4237ad019d1f9fe58a4f1501
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-03-25 00:59:58 +0200

    3.2. Add static serving

--- src/express/index.js
+++ src/express/index.js
@@ -1,13 +1,21 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const mainRoutes = require(`./routes/main.js`);
 const myRoutes = require(`./routes/my.js`);
 const articlesRoutes = require(`./routes/articles.js`);
 
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
+const TEMPLATES_DIR = `templates`;
 const app = express();
+
+app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
+app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
