c4d06aca680c33b6b4da35f638a79bc0035f20c8
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-03-31 06:28:16 +0200

    3.2. Add new-post and edit-post templates

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -6,13 +6,13 @@ const {Router} = require(`express`);
 const articlesRouter = new Router();
 
 articlesRouter.get(`/add`, (req, res) => {
-  res.send(`/articles/add`);
+  res.render(`new-post`);
 });
 articlesRouter.get(`/category/:id`, (_req, res) => {
   res.render(`articles-by-category`);
 });
 articlesRouter.get(`/edit/:id`, (req, res) => {
-  res.send(`/articles/edit/${req.params.id}`);
+  res.render(`edit-post`);
 });
 articlesRouter.get(`/:id`, (_req, res) => {
   res.render(`post`);
