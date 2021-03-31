4d6cfa0d77c322ba49c6f0679c0ace8d277f82f9
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-03-31 06:11:37 +0200

    3.2 Add articles-by-category template

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -8,13 +8,13 @@ const articlesRouter = new Router();
 articlesRouter.get(`/add`, (req, res) => {
   res.send(`/articles/add`);
 });
-articlesRouter.get(`/category/:id`, (req, res) => {
-  res.send(`/articles/category/${req.params.id}`);
+articlesRouter.get(`/category/:id`, (_req, res) => {
+  res.render(`articles-by-category`);
 });
 articlesRouter.get(`/edit/:id`, (req, res) => {
   res.send(`/articles/edit/${req.params.id}`);
 });
-articlesRouter.get(`/:id`, (req, res) => {
+articlesRouter.get(`/:id`, (_req, res) => {
   res.render(`post`);
   // res.send(`/articles/${req.params.id}`);
 });
